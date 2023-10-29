eef44e98d568ba6f9e046a27722307901c06cd21
Sachin Mane <mane.sachin29@gmail.com>
2023-10-29 18:19:33 +0530

    Update roman-to-integer.java
    https://leetcode.com/problems/roman-to-integer/

--- roman-to-integer.java
+++ roman-to-integer.java
@@ -1,7 +1,7 @@
 class Solution {
   public int romanToInt(String s) {
 
-    Map < Character, Integer > romanToInt = new HashMap < > ();
+    Map< Character, Integer > romanToInt = new HashMap < > ();
     romanToInt.put('I', 1);
     romanToInt.put('V', 5);
     romanToInt.put('X', 10);
