b7ea24e2e89865ab726e635f5fc46939236dff20
Sachin Mane <mane.sachin29@gmail.com>
2023-10-29 18:28:34 +0530

    Create two-sum.java
    https://leetcode.com/problems/two-sum/

---
+++ two-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    // {3,6} 9
+    Map < Integer, Integer > numPositionMap = new HashMap < > ();
+
+    for (int i = 0; i < nums.length; i++) {
+      if (numPositionMap.containsKey(target - nums[i])) {
+        return new int[] { numPositionMap.get(target - nums[i]), i };
+      }
+      numPositionMap.put(nums[i], i);
+    }
+    return new int[] {};
+  }
+  /* If array is sorted can use two pointers
+  int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum < target) {
+                i++;
+            } else if (sum > target) {
+                j--;
+            } else {
+                return new int[] {i, j};
+            }
+        }
+        return new int[] { -1, -1 };
+    }*/
+}
